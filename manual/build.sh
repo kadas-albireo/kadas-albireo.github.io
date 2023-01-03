d45d2209ba84ca1f56d25deba8554544fb72cc76
Sandro Mani <manisandro@gmail.com>
2023-01-03 14:02:46 +0100

    Build docs in venv

--- manual/build.sh
+++ manual/build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+echo "* Setting up venv..."
+rm -rf .venv
+python3 -mvenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo "* Clean previous build..."
 rm -rf html
 mkdir html
 
+echo "* Building..."
 LANGS="fr it de en"
 for lang in $LANGS; do
     echo "Building $lang"
@@ -13,3 +21,8 @@ done
 
 # HACK Comment out code leading to incorrect current chapter highlighting in nav tree
 sed -i 's|e.reset(),|/*e.reset(),*/|' html/js/theme.js
+
+# cleanup venv
+echo "* Clean venv..."
+deactivate
+rm -rf .venv
--- technical/build.sh
+++ technical/build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+echo "* Setting up venv..."
+rm -rf .venv
+python3 -mvenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo "* Clean previous build..."
 rm -rf html
+mkdir html
+
+echo "* Building..."
 mkdocs build -f kadas-technical.yml
 mv site html
+
+# cleanup venv
+echo "* Clean venv..."
+deactivate
+rm -rf .venv
